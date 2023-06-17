04f4bd8fb124477b7b3edaab258903620ae7d6a6
arthurkroth <kroth.sc@gmail.com>
2023-06-17 17:22:42 +0100

    17/06 class

--- JFT4Ex1.java
+++ JFT4Ex1.java
@@ -1,9 +1,7 @@
 public class JFT4Ex1 {
     public static void main(String args[]){
-        System.out.println(10+12);
-        System.out.println(10-12);
-        System.out.println(10*12);
-        System.out.println((float)10/12);
-        System.out.println(10%12);
-    }
+    /*
+    // A single line comment can be placed within a multi-line comment.
+   */
+ }
 }
